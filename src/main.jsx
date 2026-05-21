d251bea6ff826f7142ae22e77be5958b87827282
shivenhanda <shivenhanda2003@gmail.com>
2026-05-21 14:43:45 +0530

    Update: Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { inject } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
 
-inject()
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
   <StrictMode>
+    <BrowserRouter>
     <App />
-  </StrictMode>
-  </BrowserRouter>,
+    <Analytics/>
+  </BrowserRouter>
+  </StrictMode>,
 )
