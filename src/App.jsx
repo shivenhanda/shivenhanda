693c7b4ef34812dc52915946c53ea7b9545585c1
shivenhanda <shivenhanda2003@gmail.com>
2026-05-26 15:16:55 +0530

    fix:Skills

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Main from './components/Main'
 import './App.css'
 import About from './components/About'
 import Projects from './components/Projects'
-import Skills from './components/skills'
+import Skills from './components/Skills'
 
 function App() {
   return (
